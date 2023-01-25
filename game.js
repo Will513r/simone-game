53e8e762fd45e9b2941d5dbe5e298fb869694830
Will513r <111171001+Will513r@users.noreply.github.com>
2023-01-25 17:57:11 -0500

    initial commit

---
+++ game.js
@@ -0,0 +1,13 @@
+var gamePattern = [];
+
+var buttonColors = ["red", "blue", "green", "yellow"];
+
+// var randomChosenColor = buttonColors[randomNumber];
+// var randomNumber = Math.floor(Math.random() * 4);
+function nextSequence(){
+    var randomNumber = Math.floor(Math.random() * 4);
+    var randomChosenColor = buttonColors[randomNumber];
+    gamePattern.push(randomChosenColor);
+}
+
+
