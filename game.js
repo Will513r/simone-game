a201de7ff93b2ea0be191a433f070d2f27b2ebc9
will513 <will513r@gmail.com>
2023-01-26 23:39:44 -0500

    created animations to button selection

--- game.js
+++ game.js
@@ -1,19 +1,28 @@
 var gamePattern = [];
 
 var buttonColors = ["red", "blue", "green", "yellow"];
-
+ 
 // var randomChosenColor = buttonColors[randomNumber];
 // var randomNumber = Math.floor(Math.random() * 4);
 function nextSequence(){
     var randomNumber = Math.floor(Math.random() * 4);
     var randomChosenColor = buttonColors[randomNumber];
+
+
     gamePattern.push(randomChosenColor);
-    
+
     $("#" + randomChosenColor)
-      .fadeOut(100)
-      .fadeIn(100);
+    .fadeOut(100)
+    .fadeIn(100);
+
+    $("#" + randomChosenColor).on(function () {
+      $("sounds/" + randomChosenColor + ".mp3").play();
+    });
+
+
+
       }
    
  
 
-
+//  $("#" + randomChosenColor).click(function ()
